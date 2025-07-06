4f475fcba4f9c2506dbdb956e8e396874f4f3f93
Jagriti Rastogi <jagritirastogi94@gmail.com>
2025-07-06 18:34:27 +0530

    Add Changes

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// Navigation Menu Toggle
-
-const menuToggle = document.querySelector(".hamburger-lines");
+const menuToggle = document.querySelector(".navbar-container input[type='checkbox']");
 const navMenu = document.querySelector(".dance-items");
 
-menuToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("active"); // Add/Remove 'active' class to show/hide menu
+menuToggle.addEventListener("change", () => {
+  if (menuToggle.checked) {
+    navMenu.classList.add("active");
+  } else {
+    navMenu.classList.remove("active");
+}
 });
+
